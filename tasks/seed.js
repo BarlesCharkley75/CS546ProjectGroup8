975c509afeae97090efa136569d054fa2988aec1
BarlesCharkley75 <45805171+BarlesCharkley75@users.noreply.github.com>
2021-12-09 10:37:38 -0500

    updated seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -50,19 +50,145 @@ const main = async () => {
   );
   console.log(user3);
 
+  let user4 = await registeredUsers.createUser(
+    "Jerry",
+    "Seinfeld",
+    "jseinfeld@gmail.com",
+    "pfp",
+    "New York City",
+    "NY",
+    63,
+    ["New York City", "California"],
+    "jseinfe4",
+    "pass12345"
+  );
+  console.log(user4);
+
+  let user5 = await registeredUsers.createUser(
+    "George",
+    "Costanza",
+    "gcostanz@gmail.com",
+    "pfp",
+    "New York City",
+    "NY",
+    45,
+    ["New York City", "Disney World"],
+    "gcostanz8",
+    "pass12345"
+  );
+  console.log(user5);
+
+  let user6 = await registeredUsers.createUser(
+    "Elaine",
+    "Benes",
+    "ebenes@gmail.com",
+    "pfp",
+    "New York City",
+    "NY",
+    45,
+    ["New York City", "Hershey"],
+    "ebenes36",
+    "pass12345"
+  );
+  console.log(user6);
+
+  let user7 = await registeredUsers.createUser(
+    "Cosmo",
+    "Kramer",
+    "ckramer@gmail.com",
+    "pfp",
+    "New York City",
+    "NY",
+    45,
+    ["New York City", "Bass Pro Shop"],
+    "ckramer77",
+    "pass12345"
+  );
+  console.log(user7);
+
+  let user8 = await registeredUsers.createUser(
+    "Wayne",
+    "Knight",
+    "wknight@gmail.com",
+    "pfp",
+    "New York City",
+    "NY",
+    45,
+    ["New York City", "France"],
+    "wknight137",
+    "pass12345"
+  );
+  console.log(user8);
+
   const hotel1 = await hotels.createHotel(
-    "Brian",
+    "Marriot",
     "222-222-2222",
-    "http://www.brianhotel.com",
+    "http://www.marriothotel.com",
     "729 Street",
-    "San Diego",
-    "CA",
-    "11010",
+    "Hoboken",
+    "NJ",
+    "07030",
     ["Food", "Water"],
     ["Water Park"],
     "hotel_five.jpg"
   );
   console.log(hotel1);
+
+  const hotel2 = await hotels.createHotel(
+    "Four Seasons",
+    "444-234-2552",
+    "http://www.fourseasonshotel.com",
+    "Sesame Street",
+    "Hoboken",
+    "NJ",
+    "07030",
+    ["Buffet", "Water"],
+    ["Pool", "McDonalds"],
+    "hotel_one.jpg"
+  );
+  console.log(hotel2);
+
+  const hotel3 = await hotels.createHotel(
+    "Elite Lodge",
+    "893-232-5475",
+    "http://www.elitelodge.com",
+    "100 John Street",
+    "Hoboken",
+    "NJ",
+    "07030",
+    ["Master Chef", "Massage"],
+    ["Water Park", "Hibachi"],
+    "hotel_three.jpg"
+  );
+  console.log(hotel3);
+
+  const hotel4 = await hotels.createHotel(
+    "Sunset Springs",
+    "420-443-3432",
+    "http://www.sunsetsprings.com",
+    "800 Sunset Blvd",
+    "Hoboken",
+    "NJ",
+    "07030",
+    ["First Class", "Beach"],
+    ["Yacht Club"],
+    "hotel_four.jpg"
+  );
+  console.log(hotel4);
+
+  const hotel5 = await hotels.createHotel(
+    "Big Hotel",
+    "123-323-5458",
+    "http://www.bighotel.com",
+    "1000 Tony Lane",
+    "Hoboken",
+    "NJ",
+    "07030",
+    ["Pizza Parlor"],
+    ["Poker Club"],
+    "hotel_six.jpg"
+  );
+  console.log(hotel5);
 
   let review1 = await reviews.createReview(
     hotel1._id.toString(),
